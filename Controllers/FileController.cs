fdc48a47da3de78bef6171cbde8eb266fbc34ae3
Tangerie <josh.cavill01@gmail.com>
2021-07-29 13:22:48 +1000

    First commit

---
+++ Controllers/FileController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FileUploader.Controllers
+{
+    public class FileModel
+    {
+        public string FileName { get; set; }
+        public IFormFile FormFile { get; set; }
+    }
+
+    [Route("file")]
+    public class FileController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public FileController(ILogger<FileController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        [Route("upload")]
+        [HttpPost]
+        [RequestSizeLimit(10L * 1024L * 1024L * 1024L)]
+        [RequestFormLimits(MultipartBodyLengthLimit = 10L * 1024L * 1024L * 1024L)]
+        public async Task<IActionResult> UploadFile([FromForm] FileModel file)
+        {
+            try
+            {
+                _logger.LogInformation("File Recieved");
+
+                if(file == null || file.FormFile == null)
+                {
+                    _logger.LogInformation("No File");
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                }
+
+                string name = Util.SaveFile((FormFile)file.FormFile);
+                _logger.LogInformation($"{file.FileName} -> {name}");
+                return Ok(name);
+
+            } catch(Exception e)
+            {
+                _logger.LogError(e.StackTrace);
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+
+        [Route("download/{name}")]
+        [HttpGet]
+        public FileResult Download(string name)
+        {
+            return Util.GetFileResult(name);
+        }
+
+        [Route("info/{name}")]
+        [HttpGet]
+        public Util.FileData GetFileData(string name)
+        {
+            return Util.GetFileData(name);
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FileUploader
+{
+    public static class Util
+    {
+        public static readonly string UPLOAD_PATH;
+        static Random rd = new Random();
+
+        static Util() {
+            UPLOAD_PATH = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+        }
+
+        
+        public class FileData
+        {
+            public string Name { get; set; }
+            public string Mimetype { get; set; }
+        }
+
+        public static bool DoesFileExist(string file)
+        {
+            string f = Path.Combine(UPLOAD_PATH, file);
+            return File.Exists(f) && File.Exists(f + ".json");
+        }
+
+        public static FileData GetFileData(string name)
+        {
+            if(!DoesFileExist(name))
+            {
+                return null;
+            }
+
+            string f = Path.Combine(UPLOAD_PATH, name + ".json");
+            return JsonConvert.DeserializeObject<FileData>(File.ReadAllText(f));
+        }
+
+        public static string GenerateFileName()
+        {
+            const string allowedChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz0123456789";
+            char[] chars = new char[16];
+
+            for (int i = 0; i < 16; i++)
+            {
+                chars[i] = allowedChars[rd.Next(0, allowedChars.Length)];
+            }
+
+            return new string(chars);
+        }
+
+        public static FileResult GetFileResult(string name)
+        {
+            if (!DoesFileExist(name))
+            {
+                return null;
+            }
+
+            string f = Path.Combine(UPLOAD_PATH, name);
+
+            FileData data = JsonConvert.DeserializeObject<FileData>(File.ReadAllText(f + ".json"));
+
+            FileContentResult result = new FileContentResult(File.ReadAllBytes(f), data.Mimetype)
+            {
+                FileDownloadName = data.Name
+            };
+
+            return result;
+        }
+
+        public static string SaveFile(FormFile file)
+        {
+            string name = GenerateFileName();
+
+            while(DoesFileExist(name))
+            {
+                name = GenerateFileName();
+            }
+
+            string path = Path.Combine(UPLOAD_PATH, name);
+            string jsonPath = path + ".json";
+
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            FileData data = new FileData()
+            {
+                Name = file.FileName,
+                Mimetype = file.ContentType
+            };
+
+            File.WriteAllText(jsonPath, JsonConvert.SerializeObject(data));
+
+            return name;
+        }
+
+        
+    }
+}
